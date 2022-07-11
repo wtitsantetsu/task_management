5bcf4b31362dd0f39f8a12c9863d68fd4e2852ea
渡辺一徹 <watanabeittetsu@TetsunoMacBook-Air.local>
2022-07-11 15:54:29 +0900

    初めてのrails練習用

--- db/migrate/20220705073441_create_tasks.rb
+++ db/migrate/20220705073441_create_tasks.rb
@@ -1,7 +1,8 @@
 class CreateTasks < ActiveRecord::Migration[7.0]
+  
   def change
     create_table :tasks do |t|
-      t.string :name
+      t.string :name, limit: 30, null: false
       t.text :description
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_05_073441) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_11_061846) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "name", limit: 30, null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
