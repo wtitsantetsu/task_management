cc9a339b30bc892209a8996e455e96204119bdad
渡辺一徹 <watanabeittetsu@TetsunoMacBook-Air.local>
2022-07-25 19:31:29 +0900

    写真の添付とCSV形式の入出力

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,7 +5,15 @@ class TasksController < ApplicationController
     @q = current_user.tasks.ransack(params[:q])
     @tasks = @q.result(distinct: true)
     # @tasks = Task.where(user_id: current_user.id).order(created_at: :desc)
+    respond_to do |format|
+      format.html
+      format.csv { send_data @tasks.generate_csv, filename: "tasks-#{Time.zone.now.strftime('%Y%M%d%S')}.csv"}
+    end
+  end
 
+  def import
+    current_user.tasks.import(params[:file])
+    redirect_to tasks_url, notice: "タスクを追加しました"
   end
 
   def show
@@ -59,7 +67,7 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:name, :description)
+    params.require(:task).permit(:name, :description, :image)
   end
 
   def set_task
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,24 +1,46 @@
 class Task < ApplicationRecord
-    validates :name, presence: true
-    validates :name, length: { maximum: 30 }
-    validate :validate_name_not_including_comma
-    # before_validation :set_nameless_name
-    belongs_to :user
-    scope :recent, -> { order(created_at: :desc)}
+  has_one_attached :image
 
+  validates :name, presence: true
+  validates :name, length: { maximum: 30 }
+  validate :validate_name_not_including_comma
+  # before_validation :set_nameless_name
+  belongs_to :user
+  scope :recent, -> { order(created_at: :desc)}
 
-    private
-    def validate_name_not_including_comma
-        errors.add(:name,'にカンマを含めることはできません') if name&.include?(',')
+  def self.csv_attributes
+    ["name", "description", "created_at", "updated_at"]
+  end
+
+  def self.generate_csv
+    CSV.generate(headers: true) do |csv|
+      csv << csv_attributes
+      all.each do |task|
+        csv << csv_attributes.map{|attr| task.send(attr)}
+      end
     end
+  end
 
-    # ransacker :created_at do
-    #     Arel.sql('date(created_at)')
-    # end
+  def self.import(file)
+    CSV.foreach(file.path, headers: true) do |row|
+      task = new
+      task.attributes = row.to_hash.slice(*csv_attributes)
+      task.save!
+    end
+  end
 
-    # def set_nameless_name
-    #     self.name = "名前なし" if name.blank?
-    # end
+  private
+  def validate_name_not_including_comma
+      errors.add(:name,'にカンマを含めることはできません') if name&.include?(',')
+  end
+
+  # ransacker :created_at do
+  #     Arel.sql('date(created_at)')
+  # end
+
+  # def set_nameless_name
+  #     self.name = "名前なし" if name.blank?
+  # end
 
 end
 
